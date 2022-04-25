0824d1fb4d617c056f19eb9e71e51c1cbb2542e7
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2022-04-25 17:30:00 -0400

    model architecture and loss

--- code/models.py
+++ code/models.py
@@ -11,25 +11,23 @@ class YourModel(tf.keras.Model):
     def __init__(self):
         super(YourModel, self).__init__()
 
-        # TODO: Select an optimizer for your network (see the documentation
-        #       for tf.keras.optimizers)
-
         self.optimizer =tf.keras.optimizers.RMSprop(learning_rate=hp.learning_rate, momentum=hp.momentum)
-       #  self.architecture = self.architecture = [Conv2D(64, 3, padding='same', activation='relu'),
-       #                                              Conv2D(128, 3, padding='same', activation='relu'),
-       #                                              MaxPool2D(),
-       #                                              Conv2D(200,5,padding='same',strides=(2,2),activation='relu'),
-       #                                              Conv2D(250,5,padding='same',strides=(2,2),activation='relu'),
-       #                                              MaxPool2D(strides=(2,2)),
-       #                                              Dropout(0.1),
-       #                                              Flatten(),
-       #                                              #Dense(500, activation='relu'),
-       #                                              Dense(100,activation='relu'),
-       #                                              Dense(hp.num_classes,activation='softmax')]
 
         self.resnet_preprocess = tf.keras.applications.resnet.preprocess_input
         self.resnet = tf.keras.applications.resnet50.ResNet5(weights='imagenet')
-        self.classification_head = [MaxPool2D(strides=(2,2)),Flatten(),Dense(200,activation='relu'),Dense(100,activation='relu'),Dense(1,activation='sigmoid')]
+
+        # From the research paper: https://arxiv.org/pdf/1808.04754.pdf 
+        # Using a 50 layered ResNet as the
+        # base architecture, we add 3 more layers of dense connections
+        # at the end, with a final layer consisting of a single sigmoid
+        # unit.
+        self.classification_head = [
+            Flatten(),
+            Dense(32,activation='relu'),
+            Dense(32,activation='relu'),
+            Dense(32,activation='relu'),
+            Dense(1,activation='sigmoid')
+        ]
 
         for l in self.resnet.layers:
                l.trainable = False
@@ -47,9 +45,7 @@ class YourModel(tf.keras.Model):
     @staticmethod
     def loss_fn(labels, predictions):
        """ Loss function for the model. """
-       # TODO: Select a loss function for your network (see the documentation
-       #       for tf.keras.losses)
-       # lf = tf.keras.losses.SparseCategoricalCrossentropy()
-       # return lf(labels, predictions, from_logits=False)
-       return tf.keras.losses.sparse_categorical_crossentropy(labels, predictions)
-       # pass
+       # Using mean squared error will maximize the Pearson's correlation coefficient
+       # https://stats.stackexchange.com/questions/301659/mse-as-a-proxy-to-pearsons-correlation-in-regression-problems 
+       mse = tf.keras.losses.MeanSquaredError()
+       return mse(labels, predictions, from_logits=False)
