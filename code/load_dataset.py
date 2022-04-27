20fa8b4ca5fade0cea173cbaa5670f3738111b14
ezhangy <elizabeth_zhang1@brown.edu>
2022-04-26 22:28:56 -0400

    load_dataset.py

---
+++ code/load_dataset.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import hyperparameters as hp
+
+class TreepediaData(): 
+    """Class for containing the training and testing sets"""
+
+    def __init__(self, data_path): 
+        self.data_path = data_path
+
+        self.train_data = self.get_data("train")
+        self.test_data = self.get_data("test")
+
+    def get_data(self, train_or_test): 
+        dataset = tf.data.TextLineDataset("code/data/sample_text_training.txt")
+        for line in dataset.take(5): 
+            print(line)
+        return dataset 
+
+def main():
+    dataset_obj = TreepediaData("")
+
+main()
